c21b4bc13431b18a69b63c3b9c0ea36e3965fb39
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-05-15 15:22:48 +0200

    Mise en place du server

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const app = express()
+
+const app = express();
+
+//Connexion BDD
+mongoose
+    .connect(process.env.DATABASE,{
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+        })
+    .then(() => console.log('BDD connectée'))
+    .catch(err => console.log('DB CONNECTION ERROR:', err));
 
 
 // import routes
 const authRoutes = require('./routes/auth')
 
-
+//app middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+//app.use(cors()); // Autorise le partage de ressources entre origines multiples 
+if((process.env.NODE_ENV = 'development')){
+     app.use(cors({origin: `http://localhost:3000`}))
+ }
 //middleware
 app.use('/api',authRoutes)
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`L'API écoute sur le port ${port}`)
+    console.log(`L'API Shopmarket écoute sur le port ${port}`)
 });
