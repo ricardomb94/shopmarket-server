2edcb916b48bd9c15a5088f89669d31ef5c6648a
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-05-15 16:25:55 +0200

    Mise en place des variables d'environnement

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,31 @@
 const User = require("../models/User")
 
-exports.signup = (req, res) => {
-    //console.log('REQ BODY ON SIGNUP', req.body);
-    const {name, email, password} = req.body
-    User.findOne({email}).exec((err,user)=> {
-        if(user){
-            return res.status(400).json({
-                error:'Cet email existe déjà'
-            })
-        }
-    });
-    let newUser = new User({name, email, password})
+// exports.signup = (req, res) => {
+//     //console.log('REQ BODY ON SIGNUP', req.body);
+//     const {name, email, password} = req.body
+//     User.findOne({email}).exec((err,user)=> {
+//         if(user){
+//             return res.status(400).json({
+//                 error:'Cet email existe déjà'
+//             })
+//         }
+//     });
+//     let newUser = new User({name, email, password})
     
-    newUser.save((err, succes) => {
-        if(err){
-            console.log('SIGNUP ERROR', err);
-            return res.status(400).json({
-                error: err
-            });
-        }
-        res.json({
-            message: 'Inscription validée. Vous pouvez vous connecter'
-        })
-    })
- }
+//     newUser.save((err, succes) => {
+//         if(err){
+//             console.log('SIGNUP ERROR', err);
+//             return res.status(400).json({
+//                 error: err
+//             });
+//         }
+//         res.json({
+//             message: 'Inscription validée. Vous pouvez vous connecter'
+//         })
+//     })
+//  };
+
+
+exports.signup = (req, res) => {
+    //
+};
