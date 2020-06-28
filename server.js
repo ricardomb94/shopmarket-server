f6d9c6e26e3b73a5bfab7e288ce762a9abde8e43
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-06-29 01:24:23 +0200

    Config server avant la mise en ligne

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');//Une methode d'Express qui permet de lire les données des inupts de formulaire et les stocke sous forme d'objet Js accessible ds req.body
+const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser')
 require('dotenv').config();
+
 
 //Connexion BDD
 mongoose
@@ -31,6 +33,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 
+
 //app.use(cors()); // Autorise le partage de ressources entre origines multiples
 if((process.env.NODE_ENV = 'development')){
      app.use(cors({origin: `http://localhost:3000`}))
@@ -42,7 +45,16 @@ app.use('/api',categoryRoutes)
 app.use('/api',productRoutes)
 
 
-const port = process.env.PORT || 8000;
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder name
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) =>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));//relative path
+    })
+}
+
+const port = process.env.PORT || 8000; 
 app.listen(port, () => {
     console.log(`L'API Shopmarket écoute sur le port ${port}`)
 });
