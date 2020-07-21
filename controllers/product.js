9c8aa682196f9893db4c794a8972727099f1f846
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-07-21 23:56:04 +0200

    Mise en place de la route pour afficher la liste des produits lors en utilisant la bar de recherche

--- controllers/product.js
+++ controllers/product.js
@@ -266,3 +266,27 @@ exports.image = (req, res, next) => {
     }
     next();
 }
+
+
+exports.listSearch = (req, res) => {
+    //Create query object to hold search value and category value
+    const query = {}
+    //assign search value to query.name
+    if(req.query.search){
+        query.name = {$regex: req.query.search, $options:'i'}
+        //assign category value to query.category
+        if(req.query.category && req.query.category !='All'){
+            query.category = req.query.category
+        }
+        //find the product based on query objet with 2 properties
+        //search and category
+        Product.find(query, (err, products) =>{
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(products)
+        }).select('-image')
+    }
+}
